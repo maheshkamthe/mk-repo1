55a7215b247f37515d64363788705101de0d4d5e
mahesh.kamthe <mahesh.kamthe@hp.com>
2017-06-20 18:56:06 +0530

    modified - training_epochs=50

---
+++ TensorFlow-MNIST-EXT/test.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+from MNISTTester import MNISTTester
+
+####################
+# directory settings
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+data_path = '/home/ubuntu/algodataset'
+model_path = script_dir + '/models/mnist-cnn'
+
+#####################################
+# prediction test with MNIST test set
+mnist = MNISTTester(
+            model_path=model_path,
+            data_path=data_path)
+
+#mnist.accuracy_of_testset()
+#mnist.predict_random()
+
+#################################
+# prediction test with image file
+# mnist = MNISTTester(model_path)
+mnist.predict(sys.argv[1])
